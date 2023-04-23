2bfb418ba7ac171922b78fba15b4e154ef2456c0
marksikaundi <emmanuelmark117@gmail.com>
2023-04-23 19:26:48 +0200

    Updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="">
-      <div>
+      <div className="text-6xl">
         <h1>Lets build GPT-4 Weather App</h1>
       </div>
     </main>
