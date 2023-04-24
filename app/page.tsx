f2597fd7fc095b58c3b150cd98e3114d2b497350
marksikaundi <emmanuelmark117@gmail.com>
2023-04-24 15:11:00 +0200

    main page: making components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
+
 export default function Home() {
   return (
     <main className="">
       <div className="text-6xl">
-        <h1>Lets build GPT-4 Weather App</h1>
+        <Card>
+          <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
+          <Subtitle className="text-xl text-center">
+            Powered by OpenAI, Nextjs 13.3, Tailwindcss, Tremor 2.0 + More!
+          </Subtitle>
+          <Divider className="my-10" />
+          <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
+            {/* city */}
+          </Card>
+        </Card>
       </div>
     </main>
   );
