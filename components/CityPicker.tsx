dfe482bc88db41878b80ebf92dd566293a25947a
marksikaundi <emmanuelmark117@gmail.com>
2023-04-24 15:39:35 +0200

    working on cityPicker components

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client";
+import { Country, City } from 'country-state-city'
+import Select from 'react-select';
+
+const options = Country.getAllCountries().map(Country => ({
+    value: {
+        latitude: Country.latitude,
+        longitude: Country.longitude,
+        isoCode: Country.isoCode,
+    },
+    label: Country.name,
+}))
 
 function CityPicker() {
   return (
     <div>
-      
+      <Select 
+      options={options}
+      />
     </div>
   )
 }
