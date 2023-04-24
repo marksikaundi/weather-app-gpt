51c41643a133aacd63928d5f11d0ba54b1b6880e
marksikaundi <emmanuelmark117@gmail.com>
2023-04-24 16:29:49 +0200

    updates

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -1,61 +1,64 @@
 "use client";
-import { Country, City } from 'country-state-city'
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import Select from 'react-select';
-import GlobeIcon from "@heroicons/react/solid"
+import { Country, City } from "country-state-city";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Select from "react-select";
+import { GlobeIcon } from "@heroicons/react/solid";
 
 type option = {
-    value: {
-        latitude: string;
-        longitude: string;
-        isoCode: string;
-    };
-    label: string;
-} |null;
+  value: {
+    latitude: string;
+    longitude: string;
+    isoCode: string;
+  };
+  label: string;
+} | null;
 
 type cityOption = {
-    value: {
-        latitude: string;
-        longitude: string;
-        name: string;
-        stateCode: string;
-    };
-    label: string;
-} |null;
+  value: {
+    latitude: string;
+    longitude: string;
+    name: string;
+    stateCode: string;
+  };
+  label: string;
+} | null;
 
-const options = Country.getAllCountries().map(Country => ({
-    value: {
-        latitude: Country.latitude,
-        longitude: Country.longitude,
-        isoCode: Country.isoCode,
-    },
-    label: Country.name,
-}))
+const options = Country.getAllCountries().map((Country) => ({
+  value: {
+    latitude: Country.latitude,
+    longitude: Country.longitude,
+    isoCode: Country.isoCode,
+  },
+  label: Country.name,
+}));
 
 function CityPicker() {
-    const [selectedCountry, setSelectedCountry] = useState<option>(null)
-    const [selectedCity, setSelectedCity] = useState<cityOption>(null)
-    const router = useRouter();
+  const [selectedCountry, setSelectedCountry] = useState<option>(null);
+  const [selectedCity, setSelectedCity] = useState<cityOption>(null);
+  const router = useRouter();
 
-    const handleSelectedCountry = (option: option) => {
-        setSelectedCountry(option);
-        setSelectedCity(null);
-    }
+  const handleSelectedCountry = (option: option) => {
+    setSelectedCountry(option);
+    setSelectedCity(null);
+  };
 
   return (
-    <div>
-        <div>
-            <label htmlFor="country">Country</label>
-        </div>
-      <Select 
-      className='text-black'
-      value={selectedCountry}
-      onChange={handleSelectedCountry}
-      options={options}
+    <div className="space-y-4">
+        <div className="space-y-2">
+      <div className="flex items-center space-x-2 text-white">
+        <GlobeIcon className="h-5 w-5 text-white" />
+        <label htmlFor="country">Country</label>
+      </div>
+      <Select
+        className="text-black"
+        value={selectedCountry}
+        onChange={handleSelectedCountry}
+        options={options}
       />
+      </div>
     </div>
-  )
+  );
 }
 
-export default CityPicker
+export default CityPicker;
