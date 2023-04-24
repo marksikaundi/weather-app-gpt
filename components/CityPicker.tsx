9c9d87bdfcfe781e164f7671adfa7e2855ba2be5
marksikaundi <emmanuelmark117@gmail.com>
2023-04-24 16:14:40 +0200

    fixed packages

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -1,6 +1,28 @@
 "use client";
 import { Country, City } from 'country-state-city'
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import Select from 'react-select';
+import GlobeIcon from "@heroicons/react/solid"
+
+type option = {
+    value: {
+        latitude: string;
+        longitude: string;
+        isoCode: string;
+    };
+    label: string;
+} |null;
+
+type cityOption = {
+    value: {
+        latitude: string;
+        longitude: string;
+        name: string;
+        stateCode: string;
+    };
+    label: string;
+} |null;
 
 const options = Country.getAllCountries().map(Country => ({
     value: {
@@ -12,9 +34,24 @@ const options = Country.getAllCountries().map(Country => ({
 }))
 
 function CityPicker() {
+    const [selectedCountry, setSelectedCountry] = useState<option>(null)
+    const [selectedCity, setSelectedCity] = useState<cityOption>(null)
+    const router = useRouter();
+
+    const handleSelectedCountry = (option: option) => {
+        setSelectedCountry(option);
+        setSelectedCity(null);
+    }
+
   return (
     <div>
+        <div>
+            <label htmlFor="country">Country</label>
+        </div>
       <Select 
+      className='text-black'
+      value={selectedCountry}
+      onChange={handleSelectedCountry}
       options={options}
       />
     </div>
