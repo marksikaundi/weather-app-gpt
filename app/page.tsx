994c7e61c491033ea0b91cd77e4736af118c96f3
marksikaundi <emmanuelmark117@gmail.com>
2023-04-24 15:18:21 +0200

    progress home

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,17 @@ import { Card, Divider, Subtitle, Text } from "@tremor/react";
 
 export default function Home() {
   return (
-    <main className="">
-      <div className="text-6xl">
-        <Card>
-          <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
-          <Subtitle className="text-xl text-center">
-            Powered by OpenAI, Nextjs 13.3, Tailwindcss, Tremor 2.0 + More!
-          </Subtitle>
-          <Divider className="my-10" />
-          <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
-            {/* city */}
-          </Card>
+    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
+      <Card className="max-w-4xl mx-auto">
+        <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
+        <Subtitle className="text-xl text-center">
+          Powered by OpenAI, Nextjs 13.3, Tailwindcss, Tremor 2.0 + More!
+        </Subtitle>
+        <Divider className="my-10" />
+        <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
+          {/* city */}
         </Card>
-      </div>
-    </main>
+      </Card>
+    </div>
   );
 }
