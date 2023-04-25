344e25e618f2d81cf96dae0a932afac6c784afdf
marksikaundi <emmanuelmark117@gmail.com>
2023-04-25 02:09:53 +0200

    updates

--- app/location/[city]/[lati]/[long]/page.tsx
+++ app/location/[city]/[lati]/[long]/page.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+type Props = {
+    params: {
+        city: string;
+        lat: string;
+        long: string;
+    }
+}
 
-function WeatherPage() {
+
+
+function WeatherPage({params: {city, lat, long}}: Props) {
   return (
     <div>
       Welcome to Weather page
